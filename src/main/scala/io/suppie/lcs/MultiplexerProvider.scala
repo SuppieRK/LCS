a831593bd4ad9cea696a422b325e975661787768
Roman Khlebnov <roman.khlebnov@netcracker.com>
2018-02-08 09:35:59 +0300

    Small improvements

--- src/main/scala/io/suppie/lcs/MultiplexerProvider.scala
+++ src/main/scala/io/suppie/lcs/MultiplexerProvider.scala
@@ -15,13 +15,9 @@ case class MultiplexerProvider(multiplexerOrder: Int = 4) {
     */
   def generateRandomMultiplexerSignal: String = {
     require(isPowerOfTwo(multiplexerOrder), "Multiplexer order must be the power of two")
-
     val requiredInputLength = multiplexerOrder + findThePowerOfTwo(multiplexerOrder)
     val rng = ThreadLocalRandom.current()
-
-    (0 until requiredInputLength).map { i =>
-      if (rng.nextDouble() > 0.5) 1 else 0
-    }.mkString
+    (0 until requiredInputLength).map(_ => if (rng.nextDouble() > 0.5) 1 else 0).mkString
   }
 
   /**
@@ -80,21 +76,48 @@ case class MultiplexerProvider(multiplexerOrder: Int = 4) {
     * Method to find the power of two of the number
     *
     * @param i The number we are searching power for
-    * @return THe power of two
+    * @return The power of two
     */
   private def findThePowerOfTwo(i: Int): Int = {
     // Change bits in the number
+    // EXAMPLE: Let i be 2^7 -> 128 -> 10000000
     val x = i - 1
+    // EXAMPLE: Then x will be 127  -> 01111111
+
     // Split bits into pairs and write how many ones there were into a new number
     val x1 = x - ((x >> 1) & 0x55555555)
+    // EXAMPLE: 0x55555555 -> 01010101 01010101 01010101 01010101 -> 1431655765
+    // EXAMPLE: (x >> 1)                -> 63  -> 00111111
+    // EXAMPLE: ((x >> 1) & 0x55555555) -> 21  -> 00010101
+    // EXAMPLE: x1                      -> 106 -> 01101010
+
     // Repeat process, but now we split bits in groups of 4
     val x2 = (x1 & 0x33333333) + ((x1 >> 2) & 0x33333333)
+    // EXAMPLE: 0x33333333 -> 00110011 00110011 00110011 00110011 -> 858993459
+    // EXAMPLE: (x1 & 0x33333333)        -> 34 -> 00100010
+    // EXAMPLE: (x1 >> 2)                -> 26 -> 00011010
+    // EXAMPLE: ((x1 >> 2) & 0x33333333) -> 18 -> 00010010
+    // EXAMPLE: x2                       -> 52 -> 00110100
+
     // Repeat it several more times
     val x3 = (x2 + (x2 >> 4)) & 0x0F0F0F0F
+    // EXAMPLE: 0x0F0F0F0F -> 00001111 00001111 00001111 00001111 -> 252645135
+    // EXAMPLE: (x2 >> 4)        -> 3  -> 00000011
+    // EXAMPLE: (x2 + (x2 >> 4)) -> 55 -> 00110111
+    // EXAMPLE: x3               -> 7  -> 00000111
+
     val x4 = x3 + (x3 >> 8)
+    // EXAMPLE: (x3 >> 8) -> 0 -> 00000000
+    // EXAMPLE: x4        -> 7 -> 00000111
+
     val x5 = x4 + (x4 >> 16)
+    // EXAMPLE: (x4 >> 16) -> 0 -> 00000000
+    // EXAMPLE: x5         -> 7 -> 00000111
+
     // Remove excess
-    x5 & 0x0000003F
+    x5 & 0x0000007F
+    // EXAMPLE: 0x0000007F -> 01111111 -> 127
+    // EXAMPLE: 00000111 -> 7
   }
 
   /**
